bb2ab4c72c68edec50c2885478b4c0f11b3cf3f3
Koenig <lkoenigsnyder@usgs.gov>
2022-11-03 15:33:22 -0400

    add visualization step

--- analyze_data.R
+++ analyze_data.R
@@ -4,3 +4,5 @@ important_dataset <- data.frame(site_id = c("a","b","c"),
                                 temp = c(23.2, 12.1, 20.5))
 
 important_dataset$log_temp <- log(important_dataset$temp)
+
+hist(important_dataset$log_temp)
